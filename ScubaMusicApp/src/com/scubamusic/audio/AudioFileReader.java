39f528717bc9cf4b3066f1542098269ba602416a
exolemon <cellben99@gmail.com>
2024-03-15 12:34:24 -0400

    combine attempt 2

--- ScubaMusicApp/src/com/scubamusic/audio/AudioFileReader.java
+++
@@ -1,8 +0,0 @@
-// Reads audio Files (e.g., MP3, MP4, etc.) and extracts the metadata.
-
-
-package com.scubamusic.audio;
-
-public class AudioFileReader {
-
-}
--- ScubaMusicApp/src/com/scubamusic/audio/AudioPlayer.java
+++
@@ -1,8 +0,0 @@
-// Handles audio playback functionality using JavaFX MEdia API or other libraries
-
-
-package com.scubamusic.audio;
-
-public class AudioPlayer {
-
-}
