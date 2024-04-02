4825ef502ba6812a63b1977ea5fad0d7bcd79cbf
lkkaaiak <144166541+lkkaaiak@users.noreply.github.com>
2024-04-02 15:55:16 -0400

    Playlist Class Added

--- src/main/java/com2/model/Playlist.java
+++ src/main/java/com2/model/Playlist.java
@@ -1,9 +1,28 @@
 // Represents a playlist containing a collection of songs.
-
-
 package com2.model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //this is a comment
 public class Playlist {
+    private String name;
+    private List<Song> songs;
+
+    // Constructor
+    public Playlist (String name) {
+        this.name = name;
+        this.songs = new ArrayList<>();
+    }
+
+    // Method to add a song to the playlist
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    // Method to delete a song to the playlist
+    public void removeSong(Song song) {
+        songs.remove(song);
+    }
 
 }
